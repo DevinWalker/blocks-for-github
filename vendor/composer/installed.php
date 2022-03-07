7a95186608aba85656ea3683879984407d54877b
devinwalker <sdtucson@gmail.com>
2022-03-07 14:42:05 -0800

    initial commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'e52ab396f1f62945f6c53454197f6a4e3f03cd22',
+        'name' => 'dlocc/github-profile-block',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'dlocc/github-profile-block' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'e52ab396f1f62945f6c53454197f6a4e3f03cd22',
+            'dev_requirement' => false,
+        ),
+    ),
+);
