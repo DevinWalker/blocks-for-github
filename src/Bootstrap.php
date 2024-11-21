801d759def4f222d2fefde0e3f20f2f110e0cf81
devinwalker <sdtucson@gmail.com>
2024-11-20 23:20:46 -0800

    Add to git ignore

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -1,36 +1,33 @@
 <?php
 
-declare(strict_types=1);
+declare( strict_types=1 );
 
 namespace GitHubBlock;
 
-class Bootstrap
-{
+class Bootstrap {
 
-    public function init(): void
-    {
+    public function init(): void {
         $this->registerHooks();
     }
 
-    private function registerHooks(): void
-    {
-        add_action('init', [$this, 'registerBlock']);
-        add_action('admin_enqueue_scripts', [$this, 'addAdminLocalizations']);
+    private function registerHooks(): void {
+        add_action( 'init', [ $this, 'registerBlock' ] );
+        add_action( 'admin_enqueue_scripts', [ $this, 'addAdminLocalizations' ] );
+        add_action( 'admin_enqueue_scripts', [ $this, 'addAdminLocalizations' ] );
     }
 
-    public function registerBlock(): void
-    {
+    public function registerBlock(): void {
         wp_register_script(
             BLOCKS_FOR_GITHUB_SCRIPT_NAME,
-            plugins_url('build/index.js', BLOCKS_FOR_GITHUB_FILE),
+            plugins_url( 'build/index.js', BLOCKS_FOR_GITHUB_FILE ),
             BLOCKS_FOR_GITHUB_SCRIPT_ASSET['dependencies'],
             BLOCKS_FOR_GITHUB_SCRIPT_ASSET['version']
         );
 
-        wp_set_script_translations(BLOCKS_FOR_GITHUB_SCRIPT_NAME, 'blocks-for-github');
+        wp_set_script_translations( BLOCKS_FOR_GITHUB_SCRIPT_NAME, 'blocks-for-github' );
 
-        register_block_type(BLOCKS_FOR_GITHUB_DIR, [
-                'render_callback' => [$this, 'blockRenderCallback'],
+        register_block_type( BLOCKS_FOR_GITHUB_DIR, [
+                'render_callback' => [ $this, 'blockRenderCallback' ],
             ]
         );
     }
@@ -38,14 +35,13 @@ class Bootstrap
     /**
      * @throws \Exception
      */
-    public function blockRenderCallback($attributes)
-    {
-        $block = new Block($attributes);
+    public function blockRenderCallback( $attributes ) {
+        $block = new Block( $attributes );
+
         return $block->render();
     }
 
-    public function addAdminLocalizations(): void
-    {
+    public function addAdminLocalizations(): void {
         wp_localize_script(
             'blocks-for-github-block-editor-script',
             'bfgPreviews',
